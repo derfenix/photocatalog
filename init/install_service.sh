2aaaca0db0da6b06d26864a98dd07b20a3d567b7
derfenix <derfenix@gmail.com>
2019-09-10 15:00:26 +0300

    Improve service file and service installation script

--- init/install_service.sh
+++ init/install_service.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
-INIT=${1:-systemd}
+INIT="${1:-systemd}"
 
 CONFIG_PATH="${XDG_CONFIG_HOME:-$HOME/.config}"
 SETTINGS_PATH="${CONFIG_PATH}/photocatalog"
 
 SYSTEMD_UNIT_PATH="${CONFIG_PATH}/systemd/user/"
 
-if "${INIT}" == "systemd"
+if [[ "${INIT}" == "systemd" ]]
 then
     cp ./init/systemd/photocatalog.service $SYSTEMD_UNIT_PATH/photocatalog.service
     if test ! -f "${SETTINGS_PATH}"
     then
-        echo "TARGET=<specify target dir>\nMONITOR=<specify dir to monitor>\nMODE=hardlink" > "${SETTINGS_PATH}"
-        ${EDITOR} "${SETTINGS_PATH}"
+        echo "TARGET=<specify target dir>\nMONITOR=<specify dir to monitor>\nMODE=hardlink\n" > "${SETTINGS_PATH}"
+        echo "UPDATECTIME=true\n" >> "${SETTINGS_PATH}"
+        ${EDITOR:-vi} "${SETTINGS_PATH}"
         exit $?
     else
         exit 0
